c7d8897b30ee70a873f782fc3801d9faf5654565
denyslyuty <denys.lyuty@gmail.com>
2023-04-29 13:34:12 +0300

    Registration, simple log in and log out working (only for superadmin).

---
+++ accounts/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat Password'
+    })) 
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+    
+    #Placeholders are set for all the other fields ecxept password fields.
+    #Loop through all fields in the form and assign the widget attribute class to all fields in register template.
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        #With 'syper' we call the parent class's clean method.
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Passwords didn't match"
+            )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+
+from .models import Account
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split('@')[0] #We automaticly create username from email.
+            #Method 'create_user' is from the MyAccountManager model.
+            user = Account.objects.create_user(
+                first_name=first_name, last_name=last_name, email=email, username=username, password=password
+                )
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration successful!')
+
+            return redirect('accounts:register')
+    else:
+        form = RegistrationForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            # messages.success(request, 'You are now logged in') --> don't want to show it for now
+            return redirect ('home')
+        else:
+            messages.error(request, 'Invalid login data')
+            return redirect('accounts:login')
+        
+    return render(request, 'accounts/login.html')
+
+
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect ('accounts:login')
